1e69010684cfae77127de3ffe2e8c2d8c7fc7ad6
Anthony Poon <apoon@emarsys.com>
2019-12-12 13:26:21 +0800

    Added api and view for tickets

--- routes/api/notification.js
+++ routes/api/notification.js
@@ -2,43 +2,76 @@ const express = require('express');
 const router = express.Router();
 const { Expo } = require('expo-server-sdk');
 const expo = new Expo();
+const devices = require("../../cache/push-devices");
+const tickets = require("../../cache/message-tickets");
+const _ = require("lodash");
 
-router.post("/", (req, res, next) => {
+router.post("/tokens", (req, res) => {
+    const {
+        name,
+        token,
+    } = req.body;
+    devices[token] = {
+        name,
+        token
+    };
+    res.json({
+        status: "OK"
+    })
+});
+
+router.post("/", async (req, res, next) => {
     const {
         token,
-        message
+        message,
+        channelId,
+        data
     } = req.body;
     if (!Expo.isExpoPushToken(token)) {
         throw "Invalid expo token."
     }
     const msg = {
         to: token,
+        // iOS Only
+        _displayInForeground: true,
         // android 7.0 , 6, 5 , 4 + iOS Only
         sound: 'default',
         body: message,
-        data: {},
-        // Android only
-        channelId: "notification-app"
+        data: !!data ? data : {},
     };
+    // Android only. Append to message if channel id is provided
+    if (!!channelId) {
+        msg["channelId"] = channelId;
+    }
     const chunks = expo.chunkPushNotifications([msg]);
-    const tickets = chunks.map(async chunk => {
-        try {
-            const ticket = await expo.sendPushNotificationsAsync(chunk);
-            tickets.push(ticket);
-        } catch (e) {
-            console.log(e);
-        }
-    });
+    try {
+        const responses = await Promise.all(chunks.map(async (chunk) => await expo.sendPushNotificationsAsync(chunk)));
+        console.log(responses);
+        _.flatten(responses).forEach(res => tickets.push(res));
+    } catch (e) {
+        console.log(e);
+    }
     res.json({
         status: "OK"
     })
 });
 
-// router.post("/tokens", (req, res, next) => {
-//     const token = req.body;
-//     res.json({
-//         status: "ok"
-//     })
-// });
+router.get("/tickets", (req, res, next) => {
+    res.json({
+        tickets
+    })
+});
+
+router.get("/tickets/:id", async (req, res, next) => {
+    const ticketId = req.params.id;
+    const chunks = expo.chunkPushNotificationReceiptIds([ticketId]);
+    const responses = await Promise.all(chunks.map(async (chunk) => await expo.getPushNotificationReceiptsAsync(chunk)));
+    console.log(responses);
+    res.json({
+        id: ticketId,
+        receipt: responses[0]
+    })
+});
+
 
 module.exports = router;
--- routes/views/notification.js
+++ routes/views/notification.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const devices = require('../../cache/push-devices');
+const tickets = require('../../cache/message-tickets');
+const _ = require('lodash');
 
-router.get('/:token', function (req, res) {
-    const token = req.params.token;
-    res.render('notification', { token: token })
+router.get('/', function (req, res) {
+    res.render('notification', { devices: _.values(devices) })
+});
+
+router.get('/tickets', function (req, res) {
+    res.render('tickets', { tickets })
 });
 
 module.exports = router;
